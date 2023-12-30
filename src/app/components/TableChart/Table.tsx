89dd6ba8e5436aba6deac249346be0f3d53fed09
NurettinKaynar <nurettinkaynar568@gmail.com>
2023-12-30 19:29:13 +0300

    market kısmı tamamlandı

--- src/app/components/TableChart/Table.tsx
+++ src/app/components/TableChart/Table.tsx
@@ -1,6 +1,70 @@
-import React from "react";
+"use client";
+import { DataResultModelDto, MarketData } from "@/app/core/interfaces";
+import { get } from "@/app/core/services/httpEntityService";
+import { ApiUrl } from "@/app/core/utils/apiUrl";
+import { AxiosResponse } from "axios";
+import React, { useState, useEffect } from "react";
 import { Search } from "react-feather";
+import { Btc, Etc, Dao, Ltc, Anc } from "react-cryptocoins";
+import { NumberConverterService } from "@/app/core/services/numberConverter";
 const Table = () => {
+  const [MarketData, setMarketData] = useState<MarketData[] | null>(null);
+
+  const getMarkets = () => {
+    get(ApiUrl.GET_MARKETS).then(
+      (res: AxiosResponse<DataResultModelDto<MarketData[]>>) => {
+        if (res.status === 200) {
+          console.log("MarketData", res.data);
+          setMarketData(res.data.data.slice(0, 5));
+        }
+      }
+    );
+  };
+  const returnIcon = (id: string) => {
+    console.log("ID", id);
+
+    switch (id) {
+      case "bitcoin":
+        return <Btc />;
+      case "ethereum":
+        return <Etc />;
+      case "multi-collateral-dai":
+        return <Dao />;
+      case "chainlink":
+        return <Ltc />;
+      case "cosmos":
+        return <Anc />;
+    }
+  };
+
+  useEffect(() => {
+    getMarkets();
+  }, []);
+
+  const Row = ({ rowData }: { rowData: MarketData }) => {
+    return (
+      <div className=" text-light text-slate-500 text-xl w-full p-4 border-b border-b-slate-500 grid grid-rows-1 grid-cols-5 grid-flow-col ">
+        <div className="  flex flex-grow items-center gap-2">
+          {returnIcon(rowData.baseId)}
+          <span>
+            {rowData.baseId} ( {rowData.baseSymbol} )
+          </span>
+        </div>
+        <span>{NumberConverterService(Number(rowData.priceUsd))}$</span>
+        <span>{NumberConverterService(Number(rowData.volumeUsd24Hr))}$</span>
+        <span
+          className={`${
+            Number(rowData.percentExchangeVolume) > 0
+              ? "text-green-500"
+              : "text-red-500"
+          }`}>
+          {NumberConverterService(Number(rowData.percentExchangeVolume))}%
+        </span>
+        <span>{NumberConverterService(Number(rowData.tradesCount24Hr))}</span>
+      </div>
+    );
+  };
+
   return (
     <div className="rounded flex flex-col w-full bg-gray-700">
       <div className="flex justify-between items-center p-4 border-b border-b-slate-500 ">
@@ -14,23 +78,19 @@ const Table = () => {
           <Search color="gray" />
         </div>
       </div>
-      <div className="flex text-slate-500 items-center justify-evenly border-b border-b-slate-500 p-4">
+      <div className=" text-slate-500  border-b border-b-slate-500 p-4 grid grid-rows-1 grid-cols-5 grid-flow-col">
         <span className="text-light">Market</span>
         <span className="text-light">Price</span>
-        <span className="text-light">24H Change</span>
-        <span className="text-light">24H Lowest</span>
-        <span className="text-light">24H Highest</span>
-        <span className="text-light">24H Volume</span>
-        <span className="text-light">24H Value</span>
+        <span className="text-light">Volume Usd 24 Hour</span>
+        <span className="text-light">Percent Exchange Volume</span>
+        <span className="text-light">Trades Count 24 Hour</span>
       </div>
-      <div className="flex text-slate-500 items-center justify-evenly  p-4">
-        <span className="text-light">Market</span>
-        <span className="text-light">Price</span>
-        <span className="text-light">24H Change</span>
-        <span className="text-light">24H Lowest</span>
-        <span className="text-light">24H Highest</span>
-        <span className="text-light">24H Volume</span>
-        <span className="text-light">24H Value</span>
+      <div className="flex flex-col  p-4">
+        {MarketData && MarketData.length > 0
+          ? MarketData.map((item: MarketData, index: number) => (
+              <Row rowData={item} key={index} />
+            ))
+          : null}
       </div>
     </div>
   );
